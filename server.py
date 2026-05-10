d2a8f3db8522117daf545f58df39340e964c3af5
Guilherme Oliveira <gui.carvalh8@gmail.com>
2026-05-10 19:30:32 -0300

    Forcar recarga sem cache no app local

--- server.py
+++ server.py
@@ -879,6 +879,12 @@ def build_clear_session_cookie():
 
 
 class CampoSatHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=str(ROOT), **kwargs)
 
